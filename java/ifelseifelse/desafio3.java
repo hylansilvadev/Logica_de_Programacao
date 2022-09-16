cb81ca35476988f67e31360b13497a5f4c04245c
Hylan Silva <hylan.00000847839@unicap.br>
2022-09-16 11:35:11 -0300

    Mais tarde eu termino - versão: Pre-Alpha 0.0.7

---
+++ java/ifelseifelse/desafio3.java
@@ -0,0 +1,36 @@
+package java.ifelseifelse;
+
+    /*
+     *@autor: Hylan Silva
+     *@version: 0.0.6 
+     */
+
+    import java.util.Scanner;
+
+public class desafio3 {
+     // 3) Faça um programa que leia dois valores inteiros A e B se os valores forem iguais
+     //    deverá se somar os dois, caso contrário multiplique A por B. Ao final de qualquer
+     //    um dos cálculos deve-se atribuir o resultado para uma variável C e mostrar seu
+     //    conteúdo na tela.
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a,b,c;
+
+        System.out.print("Digite um número: ");
+        a = in.nextInt();
+        System.out.println("Digite outro número: ");
+        b = in.nextInt();
+
+        if(a == b){
+            c = a+b;
+            System.out.println("O valor da soma dos dois é igual a: "+c);
+        }else{
+            c = a*b;
+            System.out.println("O valor da multiplicação dos dois é igual a: "+c);
+        }
+
+        in.close();
+    }
+}
---
+++ java/ifelseifelse/desafio6.java
@@ -0,0 +1,49 @@
+package java.ifelseifelse;
+
+    /*
+     *@autor: Hylan Silva
+     *@version: 0.0.6 
+     */
+
+     import java.util.Scanner;
+
+public class desafio6 {
+
+     // 6) Escreva um programa que leia três valores inteiros e diferentes e mostre-os em
+     //    ordem crescente.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a,b,c;
+
+        System.out.println("Digite um valor: ");
+        a = in.nextInt();
+        System.out.println("Digite um outro valor: ");
+        b = in.nextInt();
+        while(a == b){
+            System.out.println("Escreva um valor diferente de: "+a);
+            System.out.println("Digite um valor: ");
+            b = in.nextInt();
+        }
+        System.out.println("Digite um ultimo valor: ");
+        c = in.nextInt();
+        while(b == c){
+            System.out.println("Escreva um valor diferente de: "+b);
+            System.out.println("Digite um valor: ");
+            c = in.nextInt();
+            while(a == c){
+                System.out.println("Escreva um valor diferente de: "+a);
+                System.out.println("Digite um valor: ");
+                c = in.nextInt();
+            }
+        }
+
+        if(a < b || b < c){
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < c; j++) {
+                    System.out.println(a);
+                }
+            }
+        }
+        
+    }
+}
